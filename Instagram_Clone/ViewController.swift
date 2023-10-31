0a990e35f4e7a7783188a7dab15770ad3efb6b3b
talhaPolat25 <easynick25@gmail.com>
2023-10-31 19:16:52 +0300

    Initial Commit

---
+++ Instagram_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_Clone
+//
+//  Created by talha polat on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

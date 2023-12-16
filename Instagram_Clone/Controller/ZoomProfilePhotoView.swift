9aa7f30dfd1a88647c8c8a18efb4a367be8c4bdd
talhaPolat25 <easynick25@gmail.com>
2023-12-16 18:04:05 +0300

    İt almost done

---
+++ Instagram_Clone/Controller/ZoomProfilePhotoView.swift
@@ -0,0 +1,20 @@
+//
+//  ZoomProfilePhotoView.swift
+//  Instagram_Clone
+//
+//  Created by talha polat on 16.12.2023.
+//
+
+import UIKit
+
+class ZoomProfilePhotoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
